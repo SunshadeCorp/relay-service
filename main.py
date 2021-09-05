542901f6480819c92a612015e523103bfc5c6bc6
Jesse Kleemann <j.kleemann@tu-bs.de>
2021-09-05 02:10:29 +0200

    precharge action

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import sys
+from time import sleep
 
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import QTimer
 from gpiozero import DigitalOutputDevice
 
 from ui.test import Ui_MainWindow
@@ -19,34 +21,59 @@ class MainWindow(Ui_MainWindow):
         self.pushButton_4.clicked.connect(self.button4)
         self.pushButton_5.clicked.connect(self.button5)
         self.pushButton_6.clicked.connect(self.button6)
+        self.pushButtonTurnOnPrecharge.clicked.connect(self.turn_on_precharge)
+        timer = QTimer(self.main_window)
+        timer.timeout.connect(self.check_states)
+        timer.start(1000)
 
     def show(self):
         self.main_window.show()
         self.app.exec_()
 
+    def turn_on_precharge(self):
+        out2.on()
+        self.check_states()
+        sleep(1)
+        out3.on()
+        self.check_states()
+        sleep(5)
+        out1.on()
+        self.check_states()
+        sleep(0.5)
+        out3.off()
+        self.check_states()
+
+    def check_states(self):
+        self.label.setText('On' if out1.is_active else 'Off')
+        self.label_2.setText('On' if out2.is_active else 'Off')
+        self.label_3.setText('On' if out3.is_active else 'Off')
+        self.label_4.setText('On' if out4.is_active else 'Off')
+        self.label_5.setText('On' if out5.is_active else 'Off')
+        self.label_6.setText('On' if out6.is_active else 'Off')
+
     def button1(self):
         out1.toggle()
-        self.label.setText('On' if out1.is_active else 'Off')
+        self.check_states()
 
     def button2(self):
         out2.toggle()
-        self.label_2.setText('On' if out2.is_active else 'Off')
+        self.check_states()
 
     def button3(self):
         out3.toggle()
-        self.label_3.setText('On' if out3.is_active else 'Off')
+        self.check_states()
 
     def button4(self):
         out4.toggle()
-        self.label_4.setText('On' if out4.is_active else 'Off')
+        self.check_states()
 
     def button5(self):
         out5.toggle()
-        self.label_5.setText('On' if out5.is_active else 'Off')
+        self.check_states()
 
     def button6(self):
         out6.toggle()
-        self.label_6.setText('On' if out6.is_active else 'Off')
+        self.check_states()
 
 
 if __name__ == '__main__':
